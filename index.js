896a4d18f60205950efaa9e9d9f43d322c103fa0
ArvinoBoro <arvin.ziba@ontariotechu.net>
2023-11-23 21:13:32 -0500

    EJS files and routes
    1. Created an EJS file for the home page, login page, teams list page, and tournament bracket page in the views folder.
    
    2. Added some basic HTML and other filler content to each EJS file.
    
    3. Created routes to each EJS file.
    
    4. Made a public directory for CSS and other static content such as images and videos.

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define a simple route
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Route for home page
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.render('home'); 
 });
 
-// Start the server
+// Route for the team list page
+app.get('/teams_list', (req, res) => {
+  res.render('teams_list'); 
+});
+
+// Route for the tournament bracket page 
+app.get('/tournament_bracket', (req, res) => {
+  res.render('tournament_bracket'); 
+});
+
+// Route for the login page 
+app.get('/login', (req, res) => {
+  res.render('login')
+});
+
+// Wildcard route for any undefined paths
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+// Starts the server 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
